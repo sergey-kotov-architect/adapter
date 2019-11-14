d742434a2b09c1acecb98f9e65a126875a1fa799
Sergey Kotov <sergey1kotov@protonmail.com>
2019-11-14 23:31:34 +0100

    override RuleSet equals() and hashCode(), set name as final

--- src/main/java/com/sergeykotov/adapter/domain/RuleSet.java
+++ src/main/java/com/sergeykotov/adapter/domain/RuleSet.java
@@ -1,20 +1,31 @@
 package com.sergeykotov.adapter.domain;
 
 import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
 
 public class RuleSet {
     @NotEmpty
-    private String name;
+    private final String name;
 
-    public RuleSet() {
+    public RuleSet(String name) {
+        this.name = name;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RuleSet ruleSet = (RuleSet) o;
+        return Objects.equals(getName(), ruleSet.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
     }
 
     @Override
