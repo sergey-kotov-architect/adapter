96ba7826cfb909c97078b210bf89cf15d9831e5f
Sergey Kotov <sergey1kotov@protonmail.com>
2019-11-14 22:28:28 +0100

    change TaskQueueException message from request queue to task queue

--- src/main/java/com/sergeykotov/adapter/exception/TaskQueueException.java
+++ src/main/java/com/sergeykotov/adapter/exception/TaskQueueException.java
@@ -3,6 +3,6 @@ package com.sergeykotov.adapter.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR, reason = "request queue is full")
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR, reason = "task queue is full")
 public class TaskQueueException extends RuntimeException {
 }
