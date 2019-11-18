dc422cbccb2bc207d93a955d7a3a1bed2a317841
Sergey Kotov <sergey1kotov@protonmail.com>
2019-11-18 02:56:03 +0100

    partially implement IntegrityService

--- src/main/java/com/sergeykotov/adapter/service/IntegrityService.java
+++ src/main/java/com/sergeykotov/adapter/service/IntegrityService.java
@@ -1,47 +1,37 @@
 package com.sergeykotov.adapter.service;
 
 import com.sergeykotov.adapter.domain.IntegrityDto;
-import com.sergeykotov.adapter.exception.ExtractionException;
-import com.sergeykotov.adapter.system.System;
-import com.sergeykotov.adapter.system.system1.System1;
-import com.sergeykotov.adapter.system.system2.System2;
+import com.sergeykotov.adapter.domain.Rule;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service
 public class IntegrityService {
     private static final Logger log = Logger.getLogger(IntegrityService.class);
-    private final List<System> systems;
+    private final RuleService ruleService;
 
     @Autowired
-    public IntegrityService(System1 system1, System2 system2) {
-        List<System> systems = new ArrayList<>();
-        systems.add(system1);
-        systems.add(system2);
-        this.systems = Collections.unmodifiableList(systems);
+    public IntegrityService(RuleService ruleService) {
+        this.ruleService = ruleService;
     }
 
     public IntegrityDto verify() {
         log.info("verifying integrity...");
-        //TODO: implement integrity verification
-        try {
-            Thread.sleep(3_000L); //latency simulation
-        } catch (InterruptedException e) {
-            log.error("integrity verification has been interrupted");
-            throw new ExtractionException();
-        }
+        String time = LocalDateTime.now().toString();
+        List<Rule> rules = ruleService.getRules();
+        boolean consistent = verify(rules);
         log.info("integrity has been verified");
 
-        String time = LocalDateTime.now().toString();
-        String message = "the business rules are consistent among the systems";
+        String success = "the business rules are consistent among the systems";
+        String fail = "integrity is violated";
+        String message = consistent ? success : fail;
+
         IntegrityDto integrityDto = new IntegrityDto();
-        integrityDto.setConsistent(true);
+        integrityDto.setConsistent(consistent);
         integrityDto.setTime(time);
         integrityDto.setMessage(message);
         return integrityDto;
@@ -53,9 +43,13 @@ public class IntegrityService {
         try {
             Thread.sleep(15_000L); //latency simulation
         } catch (InterruptedException e) {
-            log.error("integrity extraction has been interrupted");
+            log.error("integrity restoration has been interrupted");
             return;
         }
         log.info("integrity has been restored");
+    }
+
+    private boolean verify(List<Rule> rules) {
+        return true;
     }
 }
