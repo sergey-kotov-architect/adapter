ec4dbb6ef6dc1a82a6032c6f793b983d60b1f011
Sergey Kotov <sergey1kotov@protonmail.com>
2019-11-19 19:29:03 +0100

    add name to TaskType

--- src/main/java/com/sergeykotov/adapter/task/TaskType.java
+++ src/main/java/com/sergeykotov/adapter/task/TaskType.java
@@ -1,8 +1,23 @@
 package com.sergeykotov.adapter.task;
 
 public enum TaskType {
-    CREATE_RULE,
-    UPDATE_RULE,
-    DELETE_RULE,
-    RESTORE_INTEGRITY
+    CREATE_RULE("create rule"),
+    UPDATE_RULE("update rule"),
+    DELETE_RULE("delete rule"),
+    RESTORE_INTEGRITY("restore integrity");
+
+    TaskType(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
 }
